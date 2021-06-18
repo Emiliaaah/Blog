004269f1cf72a968fc4964d9f343a53d2aee545e
sanderstarr <sanderstargaming2004@gmail.com>
2021-06-18 14:48:49 +0200

    overall layout

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,14 @@ import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <div className="mx-auto w-9/12">
+      <header>
+      <h1 className="text-6xl my-8 font-bold text-center">My Blog</h1>
       <nav>
-        <ul>
+        <ul className="flex flex-row justify-center space-x-4">
           <li>
             <Link href='/'>
-              <a>Home</a>  
+              <a className="">Home</a>
             </Link>
           </li>
           <li>
@@ -18,6 +20,8 @@ function MyApp({ Component, pageProps }) {
             </li>
         </ul>
       </nav>
+      </header>
+
       <Component {...pageProps} />
     </div>
   )
